806eeafcf05c9dfc59fedaac5588c760acf279a9
Andrew GALAVAN <agalavan@student.unit.ua>
2018-03-29 17:03:00 +0300

    details page does not work

---
+++ suggest.php
@@ -0,0 +1,13 @@
+<?php 
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+
+include("inc/header.php"); ?>
+
+<div class="section catalog page">
+  
+  <h1><?php echo $pageTitle; ?></h1>
+    
+</div>
+
+<?php include("inc/footer.php"); ?>
