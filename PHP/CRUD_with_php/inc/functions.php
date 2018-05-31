a7ef8d0bc62f8f322592791a4dacb0d5c2266822
Andrew <agalavan@student.unit.ua>
2018-05-31 16:37:29 +0300

    add some sql db queries

--- PHP/CRUD_with_php/inc/functions.php
+++ PHP/CRUD_with_php/inc/functions.php
@@ -1,2 +1,44 @@
 <?php
 //application functions
+
+function get_project_list() {
+    include 'connection.php';
+    
+    try {
+        return $db->query('SELECT project_id, title, category FROM projects');
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return array();
+    }
+}
+
+function get_task_list() {
+    include 'connection.php';
+    
+    $sql = 'SELECT tasks.*, projects.title as project FROM tasks'
+        . ' JOIN projects ON tasks.project_id = projects.project_id';
+    
+    try {
+        return $db->query($sql);
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return array();
+    }
+}
+
+function add_project($title, $category){
+    include 'connection.php';
+    
+    $sql = 'INSERT INTO projects(title, category) VALUES(?, ?)';
+    
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $title, PDO::PARAM_STR);
+        $results->bindValue(2, $category, PDO::PARAM_STR);
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return true;
+}
