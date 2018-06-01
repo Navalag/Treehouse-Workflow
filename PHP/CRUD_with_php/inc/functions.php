8d49e67602e4080e34be41c315a195f0b3cf3f7a
Andrew <agalavan@student.unit.ua>
2018-06-01 15:39:12 +0300

    reports finish + add filters

--- PHP/CRUD_with_php/inc/functions.php
+++ PHP/CRUD_with_php/inc/functions.php
@@ -17,14 +17,35 @@ function get_task_list($filter = null) {
     
     $sql = 'SELECT tasks.*, projects.title as project FROM tasks'
         . ' JOIN projects ON tasks.project_id = projects.project_id';
-     
+    
+    $where = '';
+    if (is_array($filter)) {
+        switch ($filter[0]) {
+            case 'project':
+                $where = ' WHERE projects.project_id = ?';
+                break;
+            case 'category':
+                $where = ' WHERE category = ?';
+                break;
+            case 'date':
+                $where = ' WHERE date >= ? AND date <= ?';
+                break;
+        }
+    }
+    
     $orderBy = ' ORDER BY date DESC';
     if ($filter) {
         $orderBy = ' ORDER BY projects.title ASC, date DESC';
     }
     
     try {
-        $results = $db->prepare($sql . $orderBy);
+        $results = $db->prepare($sql . $where . $orderBy);
+        if (is_array($filter)) {
+            $results->bindValue(1, $filter[1]);
+            if ($filter[0] == 'date') {
+                $results->bindValue(2, $filter[2],PDO::PARAM_STR);
+            }
+        }
         $results->execute();
     } catch (Exception $e) {
         echo "Error!: " . $e->getMessage() . "<br />";
@@ -48,6 +69,22 @@ function add_project($title, $category){
         return false;
     }
     return true;
+}
+
+function get_project($project_id){
+    include 'connection.php';
+    
+    $sql = 'SELECT * FROM projects WHERE project_id = ?';
+    
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $project_id, PDO::PARAM_INT);
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return $results->fetch();
 }
 function add_task($project_id, $title, $date, $time){
     include 'connection.php';
